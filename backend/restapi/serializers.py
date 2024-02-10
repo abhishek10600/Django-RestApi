b1df5591af102c5e707a0513d527a5caa203e7c9
abhishek10600 <abhi10sheksharma@gmail.com>
2024-02-10 22:46:01 +0530

    Django DjangoRestFramework RestApi.

---
+++ backend/restapi/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+from .models import Company
+
+
+class CompanySerializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Company
+        fields = "__all__"
